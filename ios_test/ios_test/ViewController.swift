f6736a23510db19768da50692be76bc9394ee97a
Orosz Dávid <orosz.david@sonrisa.hu>
2020-06-29 15:32:25 +0200

    Initial commit

---
+++ ios_test/ios_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_test
+//
+//  Created by Orosz Dávid on 2020. 06. 29..
+//  Copyright © 2020. Orosz Dávid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
